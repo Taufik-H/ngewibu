3232d649877dcea333773b629166c57d762b6dc6
Taufik-H <taufikrpl1@gmail.com>
2022-08-12 16:00:26 +0700

    card & button

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+const Button =()=>{
+    return(
+        <>
+        <div className="p-5">
+            <button className="w-80 bg-blue-500 rounded-lg font-semibold text-white py-3">KUY</button>
+        </div>
+        </>
+    )
+}
+export default Button
